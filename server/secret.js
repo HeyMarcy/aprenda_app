72f59e5f624a279653a2d4553bef30959a0d1d15
marcy <marcy.montross@gmail.com>
2017-04-17 10:56:07 -0500

    add secret

--- server/secret.js
+++ server/secret.js
@@ -1,1 +1,4 @@
-module.exports = { CLIENT_ID: 'yourId123.apps.googleusercontent.com', CLIENT_SECRET: 'yoursecret' }
+module.exports = {
+  CLIENT_ID: '595111419019-oqqjv9tv4iuuo1q02eiollsif5k15d6m.apps.googleusercontent.com',
+  CLIENT_SECRET: 'PzMPpP_FOYQE8CzDgE5Nghwf'
+}
