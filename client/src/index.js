273693ae4b83df821e45b887b169026bb05266f1
Laura Wolfart <laurawolfart@gmail.com>
2017-04-19 12:13:23 -0400

    react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
 import { Provider } from 'react-redux';
 import store from './store';
 import './index.css';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import * as Cookies from 'js-cookie';
+
+import LoginPage from './components/login-page';
+import QuestionPage from './components/question-page';
+import App from './components/app';
+
+const routes = (
+  <Router history={browserHistory}>
+    <Route exact path='/' component={App} />
+  </Router>
+)
 
 ReactDOM.render(
   <Provider store={store}>
-      <App />
+      {routes}
   </Provider>,
   document.getElementById('root')
 );
